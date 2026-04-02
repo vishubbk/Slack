77aceb2b39690aa4d11b931a0c48d1e6f905f3ca
vishubbk <vishubbkup@gmail.com>
2026-04-02 23:13:22 +0530

    #

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -68,7 +68,7 @@ export const loginUser = async (req, res, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: "Please provide email & password" });
+      return res.status(400).json({ message: "Something is missing" });
     }
 
     const user = await User.findOne({ email });
