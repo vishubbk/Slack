2bcc9222663bfebed360e2484d0a3bbd7b37e456
vishubbk <vishubbkup@gmail.com>
2026-04-04 23:03:44 +0530

    #

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDb = async ()=>{
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("mongodb DataBase Connected Successfully")
+    console.log("MONGODB DataBase Connected Successfully")
   } catch (error) {
     console.error("DataBase Connecttion Failed",error)
   }
